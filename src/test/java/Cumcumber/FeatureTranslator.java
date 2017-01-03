3109e92650aff4078df2ef66f0535f9357531ca2
Andrew Williams <williams.andrew15@gmail.com>
2017-01-03 14:45:53 -0500

    updated Feature Translate

--- src/test/java/Cumcumber/FeatureTranslator.java
+++ src/test/java/Cumcumber/FeatureTranslator.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@Ignore
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/test/resources/donation.feature"})
+@CucumberOptions(features={"src/test/resources/donation.feature", "src/test/resources/makeEvent.feature", "src/test/resources/viewEvent.feature"})
 public class FeatureTranslator {} // hook class
