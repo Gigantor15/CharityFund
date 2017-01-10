f334d5e82a2a37f1a34bffba7a43444eb1abbfde
Andrew Williams <williams.andrew15@gmail.com>
2017-01-10 09:34:00 -0500

    added createNewEvent to the Delegate

--- src/main/java/com/oreo/charity/middleTier/Delegate.java
+++ src/main/java/com/oreo/charity/middleTier/Delegate.java
@@ -3,11 +3,14 @@ package com.oreo.charity.middleTier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.oreo.charity.beans.Event;
+
 
 @Component(value="businessDelegate")
 public class Delegate {
 
 	private MainService mainService;
+	private CreateEventService createEventService;
 	@Autowired
 	public void setUserService(MainService mainService) { this.mainService = mainService; }
 
@@ -19,7 +22,7 @@ public class Delegate {
 	}
 
 
-	public void createNewEvent(){
-		
+	public void createNewEvent(Event event){
+		createEventService.newEvent(event);
 	}
 }
